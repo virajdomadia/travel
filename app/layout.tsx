ef0f683a22b54620439124fd2c15d1b5b1aec360
virajdomadia <virajdomadia32@gmail.com>
2026-03-01 23:08:13 +0530

    style: strictly standardize typography to Inter and JetBrains Mono across entire codebase

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default async function RootLayout({
       <head>
         <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
       </head>
-      <body className={`${inter.variable} ${jetbrainsMono.variable} font-sans`}>
+      <body className={`${inter.variable} ${jetbrainsMono.variable} font-sans antialiased`}>
         <ThemeProvider initialSettings={siteSettings}>
           <PersonalizationProvider>
             <ClientLayout>
